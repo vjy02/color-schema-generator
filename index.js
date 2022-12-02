6b3f19ce8dc2fcbfee5a1dc264295506134d9771
vjy02 <108232365+vjy02@users.noreply.github.com>
2022-12-03 05:36:31 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const selectedColor= document.getElementById('seed-color')
+const colorBtn = document.getElementById('color-button')
+const modes = document.getElementById('modes')
+const colorPallet = document.getElementById('pallets')
+const container = document.getElementById('container')
+
+const count = 6
+
+function changeBorderColor() {
+  var myContainer = container;
+  var color ='rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) +  ',0.1)';
+  myContainer.style.borderColor = color;
+}
+
+function generateColors(){
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor.value.slice(1)}&mode=${modes.value}&count=${count}`)
+        .then(res=> res.json())
+        .then(data=> {
+           
+        for(let i = 0;i<data.colors.length;i++){
+            let color = (data.colors[i].hex.value)
+            let html = ''
+            html = `
+                <div class='new-color' style="background-color:${color} ;">
+                    <div class='color-hex'>
+                        <p id='hex'>${color}</p>
+                    </div>
+                </div>
+       `
+       colorPallet.innerHTML += html
+    }    
+})
+}
+
+function clear(){
+    colorPallet.innerHTML =''
+}
+
+generateColors()
+
+colorBtn.addEventListener('click' , (e)=> {
+    clear()
+    generateColors()
+    e.preventDefault()
+})
+
+
